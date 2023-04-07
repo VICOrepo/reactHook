0b6401c66705296635b33ce9f69eedfd54003a4b
VICOrepo <viral.jain.2019@ecajmer.ac.in>
2023-04-07 16:33:11 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import ClassCounter from './Components/ClassCounter';
 import HookCount from './Components/HookCount';
 import HookCounter2 from './Components/HookCounter2';
 import HookCounter3 from './Components/HookCounter3';
+import HookCounter4 from './Components/HookCounter4';
+import ClassCounterOne from './Components/ClassCounterOne';
+import HookCounterOne from './Components/HookCounterOne';
 
 function App() {
   return (
@@ -12,7 +15,10 @@ function App() {
       {/* <ClassCounter/> */}
       {/* <HookCount/> */}
       {/* <HookCounter2/> */}
-      <HookCounter3/>
+      {/* <HookCounter3/> */}
+      {/* <HookCounter4/> */}
+      {/* <ClassCounterOne/> */}
+      <HookCounterOne/>
     </div>
   );
 }
