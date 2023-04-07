d3bc94cd2a2eb1247060ae5ead8a2d7c7290a9ef
VICOrepo <viral.jain.2019@ecajmer.ac.in>
2023-04-07 12:53:29 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import ClassCounter from './Components/ClassCounter';
+import HookCount from './Components/HookCount';
+import HookCounter2 from './Components/HookCounter2';
+import HookCounter3 from './Components/HookCounter3';
 
 function App() {
   return (
     <div className="App">
-      
+      <br/>
+      {/* <ClassCounter/> */}
+      {/* <HookCount/> */}
+      {/* <HookCounter2/> */}
+      <HookCounter3/>
     </div>
   );
 }
